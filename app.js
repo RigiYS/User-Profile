35df25ec84aafcf03c93e374a2d34769d95d199b
RigiYS <rigiyuda123@gmail.com>
2025-01-12 19:20:09 +0700

    modify user-routes and app.js

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const userRouter = require("./routes/user-route");
+const userLoger = require("./middleware/user-logger")
+const userAuth = require("./middleware/user-auth")
 
 dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/api/users", userRouter);
+app.use(userLoger);
+app.use(userAuth);
 
 const port = process.env.PORT;
 
